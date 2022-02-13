f794505b301193a0a75759168bb1ab5fdcec5c67
pedrocabral95 <pedro25alves@gmail.com>
2022-02-13 00:40:34 +0000

    new
    language

--- myjs.js
+++ myjs.js
@@ -1,3 +1,11 @@
+var aLanguage = ["English","French","Portuguese"];
+var aSobre = ["Sobre","About" ,"Au sujet de Moi"];
+var aInicio = ["Início","Home","Accueil"];
+var aFamilia = ["Família","Family" ,"Famille"];
+var aDesporto = ["Deportos", "Sports" ,"Sport"];
+var aJobs = ["Empregos","Jobs" ,"Emploi"];
+var aHello = ["Bem Vindo!", "Hello World!", "Salut!"]
+
 function getForm() {
       window.location = "https://forms.gle/qV3HW7ZjKoc32Fqm6";
 }
@@ -7,35 +15,44 @@ function getGit() {
 }
 var lang = 0;
 
-function active(){
-    if (lang == 0){
-        lang = 1;
-        //english
-        
+function changeLanguage() {
+    lang++;
+    if  (lang == 3){
+        lang=0;
     }
-    if ( lang == 1){
-        lang = 2;
-        //french
-    }
-    else{
-        lang = 0
-    }
-
 }
 
 function doLanguage(){
-    if (lang == 0){
-        lang = 1;
-        //english
-    }
-    if ( lang == 1){
-        lang = 2;
-        //french
-    }
-    else{
-        lang = 0
-    }
-
+    var topnav;
+    topnav = document.getElementById("Language");
+    topnav.textContent = aLanguage[lang] ;
+    topnav = document.getElementById("home");
+    topnav.textContent =aInicio[lang] ;
+    topnav = document.getElementById("about");
+    topnav.textContent = aSobre[lang] ;
+    topnav = document.getElementById("family");
+    topnav.textContent = aFamilia[lang] ;
+    topnav = document.getElementById("jobs");
+    topnav.textContent = aJobs[lang] ;
+    topnav = document.getElementById("run");
+    topnav.textContent = aDesporto[lang];
+  
+   
 }
 
 
+
+function responsive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+   
+  }
+
+setInterval(function() {
+    responsive();
+    doLanguage();
+}, 1000);
